9d9aa28c6e16f96f800a566ad28265e050a8c9b4
albertFengHC <1030970170@qq.com>
2019-10-30 11:39:18 +0800

    10.30 应用首页

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,11 +1,11 @@
 <template>
-    <div id="about">
+    <div id="index">
         <h1>This is an about page</h1>
     </div>
 </template>
 
 <style scoped lang="stylus">
-    #about
+    #index
         width 100%
         height 100%
         margin 0
