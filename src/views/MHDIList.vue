8d43b11cf571f6dbb104b6b8eda3cb1444b3892e
albertFengHC <1030970170@qq.com>
2019-10-31 13:54:56 +0800

    10.31 隐患排查首页

--- src/views/MHDIList.vue
+++ src/views/MHDIList.vue
@@ -50,9 +50,7 @@
     export default {
         name: "MHDIList",
         data() {
-            return {
-
-            }
+            return {}
         },
         methods: {
             creatLabel() {
@@ -72,9 +70,9 @@
                         y: 'middle',
                         data: ['处理完结数', '正在处理数'],
                         textStyle: {
-                            color: '#fff'
+                            color: '#4b4b4b'
                         },
-                        show:false
+                        show: false
                     },
                     series: [
                         {
@@ -87,13 +85,13 @@
                                 normal: {
                                     show: true,
                                     position: 'center',
-                                    formatter:function (argument) {
-                                        let html='主平台应急'+"\n"+'事件统计';
+                                    formatter: function (argument) {
+                                        let html = '总数' + "\n" + '1111';
                                         return html;
                                     },
-                                    textStyle:{
+                                    textStyle: {
                                         fontSize: 15,
-                                        color:'#333'
+                                        color: '#333'
                                     }
                                 },
                                 emphasis: {
@@ -101,7 +99,7 @@
                                     textStyle: {
                                         fontSize: '20',
                                         fontWeight: 'bold',
-                                        color: '#fff',
+                                        color: '#4b4b4b',
                                     },
                                 }
                             },
@@ -114,7 +112,7 @@
                                 {value: 20, name: '处理完结数'},
                                 {value: 60, name: '正在处理数'},
                             ],
-                            color: ['#00B050','#FFC000']
+                            color: ['#00B050', '#FFC000'],
                         },
                     ]
                 };
@@ -148,23 +146,21 @@
         position relative
         text-align center
         border-bottom 2px solid #EEEEEE
-
         p
             position absolute
             color #999999
-            top 35%
-            font-size 1rem
+            top 32%
             left 10%
+            font-size 1rem
 
     .search
         text-align center
         border-bottom 2px solid #EEEEEE
         border-radius 15px
         box-shadow 0 3px 0 #d6d6d6
-
         .name, .company
             padding 10px 0
-
+            position relative
             input
                 padding 10px
                 width 70%
@@ -173,20 +169,35 @@
                 border none
                 background-color #F2F2F2
                 text-align center
+            i
+                display inline-block
+                width 21px
+                height 22px
+                background-size 100% 100%
+                position absolute
+                top 40%
+                left 35%
+
+        .name
+            i
+                background url('../assets/MHDIList/组611.png') no-repeat
+
+        .company
+            i
+                background url('../assets/MHDIList/路径946.png') no-repeat
+
 
     .boxContent
         flex 3
         display flex
         flex-direction column
         justify-content space-between
-
         .content
             flex 2
             padding 30px
             .label
                 width 100%
                 height 100%
-
         .bottom
             flex 1
             padding 15px
@@ -196,7 +207,6 @@
         display flex
         justify-content space-between
         text-align center
-
         div
             flex 1
             background-color #F2F2F2
