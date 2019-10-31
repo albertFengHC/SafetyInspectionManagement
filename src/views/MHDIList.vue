2ec4fc31d5453a76188e38c9809dd2986a06ead2
albertFengHC <1030970170@qq.com>
2019-10-31 11:36:34 +0800

    10.31 隐患排查首页

--- src/views/MHDIList.vue
+++ src/views/MHDIList.vue
@@ -1,26 +1,46 @@
 <template>
     <div id="MHDIList">
-        <div class="top">
-            <h2>隐患排查清单管理</h2>
-            <p><span><</span>应用</p>
-        </div>
-        <div class="search">
-            <div class="name">
-                <i></i>
-                <input type="text" placeholder="请输入姓名">
+        <div class="topContent">
+            <div class="top">
+                <h2>隐患排查清单管理</h2>
+                <p><span><</span>应用</p>
             </div>
-            <div class="company">
-                <i></i>
-                <input type="text" placeholder="请选择单位">
+            <div class="search">
+                <div class="name">
+                    <i></i>
+                    <input type="text" placeholder="请输入姓名">
+                </div>
+                <div class="company">
+                    <i></i>
+                    <input type="text" placeholder="请选择单位">
+                </div>
             </div>
         </div>
-        <div class="content">
-            <div class="label" ref="label"></div>
-        </div>
-        <div class="bottom">
-            <div v-for="data in dataNameList">
-                <h4>111</h4>
-                <p>{{data}}</p>
+        <div class="boxContent">
+            <div class="content">
+                <div class="label" ref="label"></div>
+            </div>
+            <div class="bottom">
+                <div class="bottomTop">
+                    <div>
+                        <h2>111</h2>
+                        <p>未按期未销号</p>
+                    </div>
+                    <div>
+                        <h2>111</h2>
+                        <p>未销号</p>
+                    </div>
+                </div>
+                <div class="bottomBottom">
+                    <div>
+                        <h2>111</h2>
+                        <p>未按期已销号</p>
+                    </div>
+                    <div>
+                        <h2>111</h2>
+                        <p>按期销号</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -29,15 +49,84 @@
 <script>
     export default {
         name: "MHDIList",
-        data(){
-          return{
-              dataNameList: ['未按期未销号','未销号','未按期已销号','按期销号']
-          }
-        },
-        methods:{
+        data() {
+            return {
 
+            }
         },
+        methods: {
+            creatLabel() {
+                const that = this;
+                const echarts = require('echarts');
+                // 基于准备好的dom，初始化echarts实例
+                const myChart = echarts.init(that.$refs.label);
 
+                let option = {
+                    tooltip: {
+                        trigger: 'item',
+                        formatter: "{b}: {c}项"
+                    },
+                    legend: {
+                        orient: 'vertical',
+                        x: 'right',
+                        y: 'middle',
+                        data: ['处理完结数', '正在处理数'],
+                        textStyle: {
+                            color: '#fff'
+                        },
+                        show:false
+                    },
+                    series: [
+                        {
+                            // name: '访问来源',
+                            type: 'pie',
+                            radius: ['60%', '90%'],
+                            center: ["50%", "50%"],
+                            avoidLabelOverlap: false,
+                            label: {
+                                normal: {
+                                    show: true,
+                                    position: 'center',
+                                    formatter:function (argument) {
+                                        let html='主平台应急'+"\n"+'事件统计';
+                                        return html;
+                                    },
+                                    textStyle:{
+                                        fontSize: 15,
+                                        color:'#333'
+                                    }
+                                },
+                                emphasis: {
+                                    show: false,
+                                    textStyle: {
+                                        fontSize: '20',
+                                        fontWeight: 'bold',
+                                        color: '#fff',
+                                    },
+                                }
+                            },
+                            labelLine: {
+                                normal: {
+                                    show: false
+                                }
+                            },
+                            data: [
+                                {value: 20, name: '处理完结数'},
+                                {value: 60, name: '正在处理数'},
+                            ],
+                            color: ['#00B050','#FFC000']
+                        },
+                    ]
+                };
+
+                // 绘制图表
+                myChart.setOption(option);
+
+            },
+        },
+        mounted() {
+            this.creatLabel();
+        }
     }
 </script>
 
@@ -46,12 +135,73 @@
         width 100%
         height 100%
         margin 0
+        color #333
         display flex
         flex-direction column
         justify-content space-between
+
+    .topContent
+        flex 1
+
+    .top
+        padding 15px
+        position relative
+        text-align center
+        border-bottom 2px solid #EEEEEE
+
+        p
+            position absolute
+            color #999999
+            top 35%
+            font-size 1rem
+            left 10%
+
+    .search
+        text-align center
+        border-bottom 2px solid #EEEEEE
+        border-radius 15px
+        box-shadow 0 3px 0 #d6d6d6
+
+        .name, .company
+            padding 10px 0
+
+            input
+                padding 10px
+                width 70%
+                border-radius 10px
+                box-shadow none
+                border none
+                background-color #F2F2F2
+                text-align center
+
+    .boxContent
+        flex 3
+        display flex
+        flex-direction column
+        justify-content space-between
+
+        .content
+            flex 2
+            padding 30px
+            .label
+                width 100%
+                height 100%
+
+        .bottom
+            flex 1
+            padding 15px
+
+
+    .bottomTop, .bottomBottom
+        display flex
+        justify-content space-between
+        text-align center
+
         div
             flex 1
-        .content
-            flex 7
+            background-color #F2F2F2
+            margin 10px
+            padding 15px
+            border-radius 5px
 
 </style>
