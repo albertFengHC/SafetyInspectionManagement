fa2f2c9a8320b1ed69c284ab60787e339a3a46a9
albertFengHC <1030970170@qq.com>
2019-10-31 18:06:51 +0800

    10.31 隐患排查清单详情页

--- src/views/LHDTCancellationDetails.vue
+++ src/views/LHDTCancellationDetails.vue
@@ -5,6 +5,35 @@
                 <h2>成宜公司【安巡查】009</h2>
                 <p @click="toLHDTCancellation"><</p>
             </div>
+            <div>
+                <p><span>被检查单位</span>TJ2-中铁四局</p>
+                <p><span>被检查记录编号</span>成宜公司【安巡查】009</p>
+                <p><span>检查时间</span>2019-09-03</p>
+            </div>
+            <div>
+                <p><span>存在隐患工程名称</span>深基坑施工</p>
+            </div>
+            <div>
+                <h3>检查发现问题</h3>
+                <div>
+                    <div>
+                        <p>103020601</p>
+                        <p>Ⅲ级</p>
+                    </div>
+                    <div>
+                        <p>1.灌注前没有二次清孔。</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <p>查看更多<span>></span></p>
+            </div>
+        </div>
+        <div class="content">
+
+        </div>
+        <div class="bottom">
+
         </div>
     </div>
 </template>
