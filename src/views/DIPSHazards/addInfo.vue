835152d05890f2f1dc09b35d619b99453ecd5f3d
albertFengHC <1030970170@qq.com>
2019-11-05 13:46:15 +0800

    11.5 安全隐患日常巡查新增页面

---
+++ src/views/DIPSHazards/addInfo.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="addInfo">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "addInfo"
+    }
+</script>
+
+<style scoped lang="stylus">
+
+</style>
